a3c40b219d7faebaf3cd9ed7ab2c6659b0d74804
Balajiudaiyar <balajiudaiyar5@gmail.com>
2026-09-21 10:05:32 +0530

    Update application

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! Application deployed automatically using GitHub Actions."
+   return "Version 2 of my application"
 
 if __name__ == "__main__":
     app.run()
